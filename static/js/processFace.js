53baa8b7ded01d3717e4aa88ebacee4fa0ec2ff0
ArpanMangal <mangalarpan@gmail.com>
2018-01-28 00:16:51 +0530

    added makeblob

--- static/js/processFace.js
+++ static/js/processFace.js
@@ -125,6 +125,28 @@ function getAttentionData(faceData) {
     }
 }
 
+function makeblob (dataURL) {
+    var BASE64_MARKER = ';base64,';
+
+    if (dataURL.indexOf(BASE64_MARKER) == -1) {
+        var parts = dataURL.split(',');
+        var contentType = parts[0].split(':')[1];
+        var raw = decodeURIComponent(parts[1]);
+        return new Blob([raw], { type: contentType });
+    }
+    var parts = dataURL.split(BASE64_MARKER);
+    var contentType = parts[0].split(':')[1];
+    var raw = window.atob(parts[1]);
+    var rawLength = raw.length;
+
+    var uInt8Array = new Uint8Array(rawLength);
+
+    for (var i = 0; i < rawLength; ++i) {
+        uInt8Array[i] = raw.charCodeAt(i);
+    }
+    return new Blob([uInt8Array], {type: contentType});
+}
+
 function processFaces(dataURL, timestamp, description, callback) {
     // **********************************************
     // *** Update or verify the following values. ***
@@ -150,13 +172,13 @@ function processFaces(dataURL, timestamp, description, callback) {
 
 
     // Perform the REST API call.
-    console.log('before ')
+    console.log('before ' + dataURL + makeblob(dataURL))
     $.ajax({
         url: uriBase + "?" + $.param(params),
 
         type: 'POST',
         processData: false,
-        contentType: 'application/octet-stream',
+        contentType: 'application/json',
 
 
         // Request headers.
@@ -166,8 +188,8 @@ function processFaces(dataURL, timestamp, description, callback) {
         },
 
         // Request body.
-        // data: '{"url": ' + '"' + sourceImageUrl + '"}'
-        data: makeblob(dataURL)
+        data: '{"url: https://pbs.twimg.com/profile_images/874276197357596672/kUuht00m_400x400.jpg"}'
+        // data: makeblob(dataURL)
 
     })
 
